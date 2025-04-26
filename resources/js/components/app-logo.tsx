729b8c2957992193f9245a910632476f2e9b73bb
Iqbal Ali <iqbalali2606@gmail.com>
2025-04-26 23:17:16 +0700

    style: sidebar and app css

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -1,13 +1,14 @@
-import AppLogoIcon from './app-logo-icon';
+import { Flower } from 'lucide-react';
+// import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
         <>
             <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-md">
-                <AppLogoIcon className="size-5 fill-current text-white dark:text-black" />
+                <Flower className="size-5  text-white dark:text-black" />
             </div>
             <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Laravel Starter Kit</span>
+                <span className="mb-0.5 truncate leading-none font-semibold">{import.meta.env.VITE_APP_NAME}</span>
             </div>
         </>
     );
--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -1,10 +1,10 @@
-import { NavFooter } from '@/components/nav-footer';
 import { NavMain } from '@/components/nav-main';
 import { NavUser } from '@/components/nav-user';
 import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+// import { NavFooter } from '@/components/nav-footer';
+import {  BookOpenTextIcon, FileText, GraduationCap, HeartHandshake, LayoutGrid, Users2 } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -13,20 +13,76 @@ const mainNavItems: NavItem[] = [
         href: '/dashboard',
         icon: LayoutGrid,
     },
+
+    // admin
+    {
+        title: 'Data Santri',
+        href: '/admin/santri ',
+        icon: Users2,
+    },
+    {
+        title: 'Data Ustadz',
+        href: '/admin/ustadz ',
+        icon: GraduationCap,
+    },
+    {
+        title: 'Data Wali Santri',
+        href: '/admin/santri ',
+        icon: HeartHandshake,
+    },
+    {
+        title: 'Data Pelajaran',
+        href: '/admin/pelajaran ',
+        icon: BookOpenTextIcon,
+    },
+    {
+        title: 'Laporan Izin Santri',
+        href: '/admin/laporan ',
+        icon: FileText,
+    },
+
+    // ustadz
+    {
+        title: 'Data Santri Didik',
+        href: '/ustadz/santri-didik',
+        icon: Users2,
+    },
+    {
+        title: 'Data Pelajaran',
+        href: '/ustadz/pelajaran',
+        icon: BookOpenTextIcon,
+    },
+    {
+        title: 'Laporan Izin Santri',
+        href: '/ustadz/izin ',
+        icon: FileText,
+    },
+
+    // wali santri
+    {
+        title: 'Data Anak',
+        href: '/wali/anak',
+        icon: Users2,
+    },
+    {
+        title: 'Surat Izin Anak',
+        href: '/wali/izin',
+        icon: BookOpenTextIcon,
+    },
 ];
 
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits',
-        icon: BookOpen,
-    },
-];
+// const footerNavItems: NavItem[] = [
+//     {
+//         title: 'Repository',
+//         href: 'https://github.com/laravel/react-starter-kit',
+//         icon: Folder,
+//     },
+//     {
+//         title: 'Documentation',
+//         href: 'https://laravel.com/docs/starter-kits',
+//         icon: BookOpen,
+//     },
+// ];
 
 export function AppSidebar() {
     return (
@@ -48,7 +104,7 @@ export function AppSidebar() {
             </SidebarContent>
 
             <SidebarFooter>
-                <NavFooter items={footerNavItems} className="mt-auto" />
+                {/* <NavFooter items={footerNavItems} className="mt-auto" /> */}
                 <NavUser />
             </SidebarFooter>
         </Sidebar>
